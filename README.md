93cc09991e8819d18965daca6fe1fb67001adc29
Edward Jiang <haojh@amazon.com>
2019-01-11 16:47:30 -0800

    Modify main to use the crypto functions and add encryption functionality

--- README.md
+++ README.md
@@ -7,6 +7,17 @@ A simple tool to help manage credentials. Inspired by vault.
 ## Example
 
 ```bash
+# Encrypt the secrets and save each in a file
+> mkdir -p secret_store/postgres
+> secmuxer -cmd encrypt -password p1ssw1rd > secret_store/postgres/host
+pg
+^d
+> secmuxer -cmd encrypt -password p1ssw1rd > secret_store/postgres/username
+readwrite
+^d
+> secmuxer -cmd encrypt -password p1ssw1rd > secret_store/postgres/password
+s.cr.t
+^d
 > tree ./secret_store/
 ./secret_store/
 └── postgres
@@ -40,14 +51,9 @@ EOF
 
 It's up to you how to store your secrets. They are just encrypted files. You can even version control them.
 
-## Bad thing
-
-1. The built in `sh` func is very dangerous. Only use this tool when you know the content of input.
-2. To leverage simplicity, it runs `openssl` to decrypt the file. And you cannot change the encryption algorithm.
-
 ## How did this come
 
-> Where do I put my credentials?
+- Where do I put my credentials?
 
 That problem concerns me everytime I build a personal tool. 
 I want everything to be version controlled, even the script of deployment.
@@ -55,3 +61,9 @@ And I also want my repo public.
 
 I know vault for a long time. It runs as a service and needs an server.
 But I just needs a simple cli tool. So I borrowed it's idea and made this.
+
+- How secure is the encrypted data?
+The code uses pbkdf2 (with SHA1 run 4096 times) to derive the encryption key from the password.
+The actual encryption is using 256-bit AES-GCM with random 96-bit nonces copied from cryptopasta, see:
+https://github.com/gtank/cryptopasta
+
--- main.go
+++ main.go
@@ -1,29 +1,53 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"strings"
 	"text/template"
 
 	"github.com/pkg/errors"
 )
 
 func main() {
+	cmd := flag.String("cmd", "extract", "Command to execute, encrypt or extract")
 	store := flag.String("store", "", "directory of the store")
 	password := flag.String("password", "", "password used to decrypt")
 	flag.Parse()
 
-	err := execute(os.Stdin, os.Stdout, *store, *password)
-	if err != nil {
-		log.Fatalln(err)
+	if *cmd == "encrypt" {
+		err := encryptSecret(os.Stdin, os.Stdout, *password)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	} else if *cmd == "extract" {
+		err := execute(os.Stdin, os.Stdout, *store, *password)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	} else {
+		log.Fatalf("Invalid command %s, only encrypt or extract is supported.", cmd)
 	}
+}
+
+func encryptSecret(in io.Reader, out io.Writer, password string) error {
+	plain, err := ioutil.ReadAll(in)
+	if err != nil {
+		return err
+	}
+
+	encrypted, err := encrypt(bytes.TrimSpace(plain), password)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(out, "%s", encrypted)
+	return nil
 }
 
 func execute(in io.Reader, out io.Writer, store, password string) error {
@@ -31,12 +55,15 @@ func execute(in io.Reader, out io.Writer, store, password string) error {
 		return filepath.Join(store, filename)
 	}
 	secret := func(filename string) (string, error) {
-		return sh("openssl aes-256-cbc -a -d -in %s -out - -pass pass:%s", resolve(filename), password)
+		encrypted, err := ioutil.ReadFile(resolve(filename))
+		if err != nil {
+			return "", err
+		}
+		plain, err := decrypt(string(encrypted), password)
+		return string(plain), err
 	}
 	funcMap := template.FuncMap{
-		"resolve": resolve,
-		"sh":      sh,
-		"secret":  secret,
+		"secret": secret,
 	}
 
 	bs, err := ioutil.ReadAll(in)
@@ -50,16 +77,4 @@ func execute(in io.Reader, out io.Writer, store, password string) error {
 	}
 
 	return errors.Wrap(t.Execute(out, nil), "could not execute")
-}
-
-func sh(cmdformat string, a ...interface{}) (string, error) {
-	shCmd := fmt.Sprintf(cmdformat, a...)
-	cmd := exec.Command("sh", "-c", shCmd)
-	cmd.Stderr = os.Stderr
-	stdout, err := cmd.Output()
-	if err != nil {
-		return "", errors.Wrapf(err, "could not run sh command: %s", shCmd)
-	}
-
-	return strings.TrimSpace(string(stdout)), nil
 }
